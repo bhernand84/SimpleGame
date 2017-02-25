4dc4de5fd495501ab353c899c5f3b2cc21fe7f47
Benaldo Hernandez <benaldohernandez@Benaldos-MBP.fios-router.home>
2017-02-25 11:00:30 -0500

    created models and updated gitignore

---
+++ SimpleGame.Web/app/simpleGame/board/board.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Board } from '../../shared/models/board';
+import { Column } from '../../shared/models/column';
+
+@Component({
+	selector: 'connect4',
+	template: `
+		<div class="board">
+			<h2>BOARD CONTAINER</h2>
+			<column (columnClickedEmitter)="onColumnClick($event)"></column>
+
+		</div>
+	`,
+	styles: [`
+		.board {
+			border: 2px solid red;
+		}
+		h2 {
+			color: red;
+		}
+	`]
+})
+export class BoardComponent {
+	@Input() board: Board;
+	@Output() columnClickedEmitter = new EventEmitter();
+
+	onColumnClick(column) {
+		//push the clicked space up to parent component
+		this.columnClickedEmitter.emit(column)
+	}
+} 
---
+++ SimpleGame.Web/app/simpleGame/space/space.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { Space } from '../../shared/models/space';
+import { Connect4API } from '../../services/connect4API';
+import { Helpers } from '../../services/helpers';
+
+@Component({
+	selector: "space",
+	template: `
+		<div class="space">
+			[O]
+		</div>
+	`,
+	styles: [`
+		.space {
+			border: 1px solid green;
+			background: green;
+			width: 60px;
+			height: 60px;
+			display: block;
+			text-align: center;
+			vertical-align: middle;
+		}
+	`]
+})
+export class SpaceComponent{
+
+}
