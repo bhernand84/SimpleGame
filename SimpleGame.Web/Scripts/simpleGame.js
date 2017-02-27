0cbef217d348e6c650b55404c25af77da53e6480
justinroyer <jroyer1986@gmail.com>
2017-02-27 15:11:47 -0800

    new index

---
+++ SimpleGame.Web/Scripts/simpleGame.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    var hub = $.connection.gameHub;
+
+    $.connection.hub.logging = true;
+    $.connection.hub.url = 'http://simplegameweb.azurewebsites.net/signalr';
+    hub.client.update = function (game) {
+        console.log(game);
+    };
+    console.log(connection);
+    $.connection.hub.start({ withCredentials: false });
+})
